e0362825bbe3f052661c3cccadeb52725efb58a0
Humann-Nehrke Sebastian <bastihumann@googlemail.com>
2025-04-03 13:57:19 +0200

    Update documentation

--- Sources/AsyncRecorder/Publisher+Recorder.swift
+++ Sources/AsyncRecorder/Publisher+Recorder.swift
@@ -8,6 +8,10 @@ import Combine
 import Foundation
 
 public extension Publisher {
+    
+    /// Create a `AsyncRecorder` for a given `Publisher`
+    /// - Parameter timeout: timeout to wait for an expected event since the last value was published. default: 1 second
+    /// - Returns: a new `AsyncRecorder`
     func record(timeout: RunLoop.SchedulerTimeType.Stride = .seconds(1)) -> AsyncRecorder<Output, Failure> {
         .init(publisher: self, timeout: timeout)
     }
